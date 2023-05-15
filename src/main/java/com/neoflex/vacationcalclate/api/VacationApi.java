1cde48339bd0fdb0f0b67d57e108e0c2ba4a0f1b
Pycuk <kha4etlov.ruslan@yandex.ru>
2023-05-15 18:27:43 +0300

    Контроллер

--- src/main/java/com/neoflex/vacationcalclate/api/VacationApi.java
+++ src/main/java/com/neoflex/vacationcalclate/api/VacationApi.java
@@ -4,8 +4,10 @@ import com.neoflex.vacationcalclate.function.CalculateFunction;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -29,7 +31,6 @@ public class VacationApi {
         }else {
             logger.warn("Incorrect data was entered");
             return 0;
-
         }
     }
 }
